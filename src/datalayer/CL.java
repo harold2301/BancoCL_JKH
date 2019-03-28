12f075135ee6b972ff06e6dd9a66c69168948eb1
harold2301 <hcordobaa@ucenfotec.ac.cr>
2019-03-27 22:18:50 -0600

    2.6

--- src/datalayer/CL.java
+++ src/datalayer/CL.java
@@ -10,4 +10,22 @@ public class CL {
     public CL() {
         this.cuentas = new ArrayList();
     }
+    
+    public void agregarCuenta(Cuenta nuevaCuenta) {
+        this.cuentas.add(nuevaCuenta);
+    }
+    
+    public ArrayList<Cuenta> getCuentas() {
+        return cuentas;
+    }
+    
+    public int buscarCuenta(String numero) {
+        for (int i = 0; i < cuentas.size(); i++) {
+            if (numero.equals(cuentas.get(i).getNumero())) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
 }
--- src/transferlayer/Controller.java
+++ src/transferlayer/Controller.java
@@ -1,13 +1,28 @@
 package transferlayer;
 
 import baselayer.CuentaAhorroProgramado;
+import baselayer.CuentaCorriente;
 import datalayer.CL;
 
 //hola prros
 public class Controller {
     private final CL logica = new CL();
     
-    public void enviarCuenta() {
-        CuentaAhorroProgramado nuevaCuenta = new CuentaAhorroProgramado(); 
+    public int enviarCuenta(String numero, double monto) {
+        
+        int pos = logica.buscarCuenta(numero);
+        
+        if (pos != -1) {
+            CuentaCorriente cuenta = (CuentaCorriente)logica.getCuentas().get(pos);
+
+            CuentaAhorroProgramado nuevaCuenta = new CuentaAhorroProgramado(cuenta);
+            nuevaCuenta.setNumero(numero);
+            nuevaCuenta.setSaldo(monto);
+
+            logica.agregarCuenta(nuevaCuenta);
+        } 
+        
+        return pos;
+        
     }
 }
