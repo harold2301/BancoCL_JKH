8f9eb675674d78813831c0ecd6d426a3c6489c69
KrauseKelsen <kperezc@ucenfotec.ac.cr>
2019-03-27 22:12:11 -0600

    V2.5

--- src/datalayer/CL.java
+++ src/datalayer/CL.java
@@ -2,12 +2,45 @@ package datalayer;
 
 import java.util.ArrayList;
 import baselayer.Cuenta;
+import baselayer.CuentaAhorro;
+import baselayer.CuentaCorriente;
 
 public class CL {
-    
+
     ArrayList<Cuenta> cuentas;
-    
+
     public CL() {
         this.cuentas = new ArrayList();
     }
+
+    public int validarCuentaCorriente(CuentaCorriente CC) {
+
+        for (int i = 0; i < cuentas.size(); i++) {
+
+            if (CC.getNumero().equalsIgnoreCase(cuentas.get(i).getNumero())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void registrarCuentaCorriente(CuentaCorriente CC) {
+        cuentas.add(CC);
+    }
+
+    public int validarCuentaAhorro(CuentaAhorro CA) {
+
+        for (int i = 0; i < cuentas.size(); i++) {
+
+            if (CA.getNumero().equalsIgnoreCase(cuentas.get(i).getNumero())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void registrarCuentaAhorro(CuentaAhorro CA) {
+        cuentas.add(CA);
+    }
+
 }
--- src/transferlayer/Controller.java
+++ src/transferlayer/Controller.java
@@ -1,32 +1,55 @@
 package transferlayer;
 
-<<<<<<< HEAD
+import baselayer.CuentaAhorro;
 import baselayer.CuentaCorriente;
-=======
 import baselayer.CuentaAhorroProgramado;
->>>>>>> 0695dd1d78bc80f322e4d007426dd3b5e407d009
 import datalayer.CL;
 
 //hola prros
+
+
 public class Controller {
+
     private final CL logica = new CL();
 
     public Controller() {
-    
-<<<<<<< HEAD
     }
-    
-    public int enviarCuentaCorriente (String numeroCuenta, double saldoInicial){
-        int valida = 0;
-        
-        
-        CuentaCorriente CC = new CuentaCorriente ();
+
+    public int enviarCuentaCorriente(String numeroCuenta, double saldoInicial) {
+        int valida;
+
+        CuentaCorriente CC = new CuentaCorriente();
         CC.setNumero(numeroCuenta);
         CC.setSaldo(saldoInicial);
+        
+        
+        valida = logica.validarCuentaCorriente(CC);
+        if (valida == -1) {
+            logica.registrarCuentaCorriente(CC);
+            
+        }
         return valida;
-=======
+
+    }
+    
+    public int enviarCuentaAhorro(String numeroCuenta, double saldoInicial) {
+        int valida;
+
+        CuentaAhorro CA = new CuentaAhorro();
+        CA.setNumero(numeroCuenta);
+        CA.setSaldo(saldoInicial);
+        
+        
+        valida = logica.validarCuentaAhorro(CA);
+        if (valida == -1) {
+            logica.registrarCuentaAhorro(CA);
+            
+        }
+        return valida;
+
+    }
+
     public void enviarCuenta() {
-        CuentaAhorroProgramado nuevaCuenta = new CuentaAhorroProgramado(); 
->>>>>>> 0695dd1d78bc80f322e4d007426dd3b5e407d009
+        CuentaAhorroProgramado nuevaCuenta = new CuentaAhorroProgramado();
     }
 }
